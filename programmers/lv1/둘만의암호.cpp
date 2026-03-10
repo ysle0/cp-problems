8bdeb1b799cd9a4643cd2d008b236ad5afb2b61a
Yoonsang Lee <ysle@pm.me>
2026-03-11 01:41:43 +0900

    add: programmers lv1 둘만의암호

---
+++ programmers/lv1/둘만의암호.cpp
@@ -0,0 +1,35 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/155652
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+    unordered_set<char> skipSet(skip.begin(), skip.end());
+    string answer = "";
+    for (char c: s) {
+        int cur = index;
+        while (cur > 0) {
+            while (skipSet.find(c + 1) != skipSet.end()) {
+                c++;
+            }
+            
+            if (c == 'z') {
+                c = 'a';
+            } else {
+                c++;
+            } 
+            
+            while (skipSet.find(c) != skipSet.end()) {
+                c++;
+            }
+            
+            cur--;
+        }
+        answer += c;
+    }
+    
+    return answer;
+}
